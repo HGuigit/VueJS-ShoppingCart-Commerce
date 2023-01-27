c98c6adca4116bbf493eadcfab0a58188baf02e5
hguigit <guilherme.silva@technosoftware.com.br>
2023-01-26 23:29:41 -0300

    Adicionando e removendo items do carrinho, dando fetch nos produtos

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="nav">
     <router-link to="/">Início</router-link> -
-    <router-link to="/basket">Carrinho (0)</router-link> 
+    <router-link to="/basket">Carrinho ({{ bagProducts.length }})</router-link>
   </div>
-  <router-view/>
+  <router-view />
 </template>
 
 <script>
+import { mapState } from "vuex";
 
-  export default {
-
-  }
-  
+export default {
+  computed: mapState({
+    bagProducts: (state) => state.bagProducts,
+  }),
+  created() {
+    this.$store.dispatch("loadProducts");
+  },
+};
 </script>
-
 
 <style lang="scss">
 #app {
@@ -36,7 +40,6 @@
   text-align: center;
   background-color: rgb(37, 37, 37);
   color: white;
-
 
   a {
     color: white;
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
   state: {
-    
+    products: [],
+    bagProducts: [],
   },
   mutations: {
-    
+    mutateProducts(state, products) {
+      state.products = products;
+    },
+    mutateBagProducts(state, product) {
+      state.bagProducts.push(product);
+    },
+    mutateRemoveBagProduct(state, product) {
+      state.bagProducts = state.bagProducts.filter(
+        (item) => item.id !== product.id
+      );
+    },
   },
   actions: {
-
+    loadProducts({ commit }) {
+      axios.get("https://fakestoreapi.com/products").then((res) => {
+        commit("mutateProducts", res.data);
+      });
+    },
+    addToBag({ commit }, product) {
+      commit("mutateBagProducts", product);
+    },
+    removeFromBag({ commit }, product) {
+      commit("mutateRemoveBagProduct", product);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
